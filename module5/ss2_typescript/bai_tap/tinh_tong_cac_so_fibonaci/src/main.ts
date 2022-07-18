60c9e78565e86ab68ae2ef0eb160d324e57e2ca3
unknown <you@example.com>
2022-07-18 14:19:37 +0700

    first commit

---
+++ module5/ss2_typescript/bai_tap/tinh_tong_cac_so_fibonaci/src/main.ts
@@ -0,0 +1,26 @@
+import defineProperty = Reflect.defineProperty;
+
+function Fibonaci(fibonaci: number): number {
+    var a = 0;
+    var b = 1;
+    var next = 0;
+    for (let i = 0; i <= fibonaci; i++) {
+        if (i <= 1) {
+            next = i;
+        } else {
+            next = a + b;
+            a = b
+            b = next
+        }
+    }
+    return next;
+}
+function Sum(sumFibonaci: number): number {
+    var sum = 0;
+    for (let i=0; i<sumFibonaci; i++){
+        sum +=Fibonaci(i);
+    }
+    return sum;
+}
+var stt: number = prompt("Nhập số lượng số Fibonaci cần tính tổng: ");
+console.log("Tổng các số fibonaci là: " + Sum(stt));
