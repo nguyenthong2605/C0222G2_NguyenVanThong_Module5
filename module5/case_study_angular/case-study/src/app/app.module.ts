813e58b80ac165c6a3eafe91ed52f448b97a703a
unknown <you@example.com>
2022-07-20 23:54:53 +0700

    first commit

--- module5/case_study_angular/case-study/src/app/app.module.ts
+++ module5/case_study_angular/case-study/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FacilityListComponent } from './facility/facility-list/facility-list.component';
+import { FacilityEditComponent } from './facility/facility-edit/facility-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    FacilityListComponent,
+    FacilityEditComponent
   ],
   imports: [
     BrowserModule,
